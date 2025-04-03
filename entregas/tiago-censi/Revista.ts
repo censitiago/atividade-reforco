5c61abeac9da4931171d4d8780f8e62c8fe6f2b8
unknown <tiagocensi132@gmail.com>
2025-04-03 11:16:29 -0300

    criei classe livro, revista e materialeitura

---
+++ entregas/tiago-censi/Revista.ts
@@ -0,0 +1,18 @@
+import { MaterialLeitura } from "./MaterialLeitura";
+
+class Revista implements MaterialLeitura {
+    titulo: string;
+    autor: string;
+    edicao: number;
+
+    constructor(titulo: string, autor: string, edicao: number) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.edicao = edicao;
+    }
+
+    exibirdetalhe(): void {
+        console.log(`Informaçoes Revista: titulo ${this.titulo} autor: ${this.autor} edicao: ${this.edicao}`)
+    }
+
+}
